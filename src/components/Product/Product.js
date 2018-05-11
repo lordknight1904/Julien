3a3a6d79bcc349b2127921014f300f707158cb89
Khoa <lordknight@Khoas-MacBook-Pro.local>
2018-05-11 16:19:58 +0700

    11/5 3

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -85,6 +85,7 @@ class Product extends Component {
             <Grid item md sm xs={12}>
               <Map
                 isMarkerShown
+                googleMapURL='https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places'
                 loadingElement={<div style={{ height: '100%' }} />}
                 containerElement={<div style={{ height: '400px' }} />}
                 mapElement={<div style={{ height: '100%' }} />}
